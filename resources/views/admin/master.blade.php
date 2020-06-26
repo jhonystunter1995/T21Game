fba21d0d4bd8b055cab3ece8ce1fa07426f3e355
jhony tabares <jhonyrust96@gmail.com>
2020-06-26 15:46:01 -0500

    css y terminacion del sidebar

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -15,10 +15,28 @@
 
             {{-- ESTILO DE LETRA ROBOTO --}}
             <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+
+            {{-- FUENTE DE ICONOS --}}
+            <script src="https://kit.fontawesome.com/cbe8f70722.js" crossorigin="anonymous"></script>
+
+            {{-- LIBRERIA JQUERRY --}}
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+            {{-- LIBRERIA BOOTSTRAP DE JS --}}
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+            {{-- INICIADOR DE JQUERRY --}}
+            <script>
+                $(document).ready(function(){
+                    $('[data-toggle="tooltip"]').tooltip()
+                });
+            </script>
             
     </head>
     <body>
 
+        {{-- LLAMADO DE CONTENEDOR SIDEBAR.. --}}
         <div class="wrapper">
             <div class="col1">@include('admin.sidebar')</div>
             <div class="col2"></div>
--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,3 +1,27 @@
 <div class="sidebar shadow">
+    <div class="section-top">
+        <div class="logo">
+            <img src="{{ url('static/images/logo1.png') }}" class="img-fluid">
+        </div>
 
+        {{-- LLamado de nombre de usuarui|email|boton de logout con icono|tooltip --}}
+        <div class="user">
+            <span class="subtitle">Hola</span>
+            <div class="name">
+                {{ Auth::user()->name }} 
+                <a href="{{ url('/logout') }}" data-toggle="tooltip" data-placement="top" title="Salir"><i class="fas fa-door-open"></i></a>
+            </div>
+            <div class="email">{{ Auth::user()->email }}</div>
+
+        </div>
+
+    </div> 
+
+    <div class="main">
+        <ul>
+        <li><a href="{{ url('/admin') }}"><i class="fas fa-chalkboard"></i> Dashboard</a></li>
+        <li><a href="{{ url('/admin/products') }}"><i class="fas fa-box-open"></i> Productos</a></li>
+        <li><a href="{{ url('/admin/users') }}"><i class="fas fa-users"></i> Usuarios</a></li>
+        </ul>
+    </div>
 </div>
