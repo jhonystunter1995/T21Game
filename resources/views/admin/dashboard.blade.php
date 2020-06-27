125868cf6634f2723bdb3c6769c19b669dffdb2e
jhony tabares <jhonyrust96@gmail.com>
2020-06-27 00:19:11 -0500

    terminado de plantilla master, css-Dashboard, maquetado, fondo

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,21 @@
-
+{{-- LLAMADO DE LA RAMA MASTER.BLADE.PHP --}}
 @extends('admin.master')
+
+{{-- TITULO DE LA PAGINA --}}
+@section('title', 'Dashboard')
+
+{{-- PANEL DE CONTENIDO DASHBOARD --}}
+@section('content')
+<div class="container-fluid">
+    <div class="panel shadow">
+        <div class="header">
+            <h2 class="title"><i class="fas fa-chalkboard"></i> Dashboard</h2>
+        </div>
+
+        <div class="insider">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae perferendis deserunt asperiores modi quam ea quae, aperiam animi aspernatur, facere ab beatae mollitia sequi adipisci vitae velit quibusdam ducimus. Nemo!
+        </div>
+    </div>
+</div>
+@endsection
 
--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -3,7 +3,7 @@
     <head>
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <title>@yield('title')</title>
+            <title>@yield('title')- T21Game</title>
             <meta name="csrf-token" content="{{ csrf_token() }}">
             <meta name="routeName" content="{{ Route::currentRouteName() }}">
             
@@ -29,19 +29,81 @@
             {{-- INICIADOR DE JQUERRY --}}
             <script>
                 $(document).ready(function(){
-                    $('[data-toggle="tooltip"]').tooltip()
+                $('[data-toggle="tooltip"]').tooltip()
                 });
             </script>
             
     </head>
     <body>
+        {{-- VIDEO DE FONDO --}}
+        <header class="v-header container">
+            <div class="fullscreen-video-wrap">
+              <video src="static/images/intro3.mp4" autoplay muted loop></video>
+            </div>
 
-        {{-- LLAMADO DE CONTENEDOR SIDEBAR.. --}}
-        <div class="wrapper">
-            <div class="col1">@include('admin.sidebar')</div>
-            <div class="col2"></div>
-        </div>
+            <div class="header-overlay"></div>
 
+            {{-- LLAMADO DE CONTENEDOR SIDEBAR.. --}}
+            <div class="wrapper">
+                <div class="col1">@include('admin.sidebar')</div>
+                
+                {{-- BARRA DE NAVEGACION --}}
+                <div class="col2">
+                    <nav class="navbar navbar-expand-lg shadow">
+                        <div class="collapse navbar-collapse">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a href="{{ url('/admin') }}" class="nav-link">
+                                        <i class="fas fa-chalkboard"></i> Dashboard
+                                    </a>
+                                </li>
+                            
+                            </ul> 
+                        </div>   
+                    </nav>
+
+                    <div class="page">
+                        <div class="container-fluid">
+                            <nav aria-label="breadcrumb shadow">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item">
+                                    <a href="{{ url('/admin') }}"><i class="fas fa-chalkboard"></i> Dashboard</a>
+                                    </li>
+                                    @section('breadcrumb')
+                                    @show
+                                </ol>
+                            </nav>
+                        </div>
+
+                        {{-- PANEL DE MENSAJES DE ERROR --}}
+                        @if(Session::has('message'))
+                          <div class="container-fluid">
+                                <div class="alert alert-{{ Session::get('typealert') }} jump1"style="display:
+                                    block; margin-bottom: 16px;">
+                                    {{ Session::get('message') }}
+                                    @if ($errors->any())
+                                    <ul>
+                                        @foreach($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                    @endif
+                                    <script>
+                                        $('.alert').slideDown();
+                                        setTimeout(function(){ $('.alert').slideup(); }, 10000);
+                                    </script>
+                                </div>
+                            </div>
+                        @endif
+
+                        {{-- LLAMADO DE LA PLANTILLA DASHBOARD.BLADE.PHP --}}
+                        @section('content')
+                        @show
+
+                    </div>
+                </div>
+            </div>
+        </header>
     </body>
 </html>
 
